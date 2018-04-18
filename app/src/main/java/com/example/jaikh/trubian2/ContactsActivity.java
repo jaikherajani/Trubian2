1a98096955fd46a101188190af71d950fcb299dd
Jai Kumar Kherajani <jai.kherajani19@gmail.com>
2018-04-18 19:51:05 +0530

    Thirtieth Commit, Bumped app version to 2.0, added contacts feature.

--- app/src/main/java/com/example/jaikh/trubian2/ContactsActivity.java
+++ app/src/main/java/com/example/jaikh/trubian2/ContactsActivity.java
@@ -1,8 +1,21 @@
 package com.example.jaikh.trubian2;
 
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class ContactsActivity extends BaseActivity {
+
+    RecyclerView recyclerView;
+    RecyclerAdapter2 mAdapter;
+    ArrayList<String> namesList = new ArrayList<>();
+    ArrayList<Integer> imageList = new ArrayList<>();
+    ArrayList<String> designationList = new ArrayList<>();
+    ArrayList<String> phoneList = new ArrayList<>();
+    ArrayList<String> emailList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -10,5 +23,18 @@ public class ContactsActivity extends BaseActivity {
         setContentView(R.layout.activity_contacts);
 
         setTitle(getResources().getString(R.string.app_name) + " : Contacts");
+
+        Collections.addAll(namesList, "Amit Saxena", "Jai Kherajani");
+        Collections.addAll(designationList, "HOD, CSE", "Student");
+        Collections.addAll(phoneList, "9229221068", "9630678955");
+        Collections.addAll(emailList, "amitsaxena@trubainstitute.ac.in", "jai.kherajani19@gmail.com");
+        //Collections.addAll(imageList, R.drawable.news_feed, R.drawable.time_table, R.drawable.calendar, R.drawable.resources, R.drawable.contacts, R.drawable.about, R.drawable.maps, R.drawable.courses, R.drawable.mis, R.drawable.others);
+
+        mAdapter = new RecyclerAdapter2(ContactsActivity.this, namesList, imageList, designationList, phoneList, emailList);
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.hasFixedSize();
+        System.out.println("ContactsActivity: " + mAdapter.getItemCount());
+        recyclerView.setAdapter(mAdapter);
     }
 }
---
+++ app/src/main/java/com/example/jaikh/trubian2/RecyclerAdapter2.java
@@ -0,0 +1,110 @@
+package com.example.jaikh.trubian2;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.drawee.view.SimpleDraweeView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Jai on 01-10-2017.
+ */
+
+public class RecyclerAdapter2 extends RecyclerView.Adapter<RecyclerAdapter2.RecyclerHolder2> {
+
+    Context context;
+    private ArrayList<String> namesList, designationList, phoneList, emailList;
+    private ArrayList<Integer> imageList;
+
+    public RecyclerAdapter2(Context context, ArrayList<String> namesList, ArrayList<Integer> imageList, ArrayList<String> designationList, ArrayList<String> phoneList, ArrayList<String> emailList) {
+        this.namesList = namesList;
+        this.imageList = imageList;
+        this.designationList = designationList;
+        this.phoneList = phoneList;
+        this.emailList = emailList;
+        this.context = context;
+        for (String name : this.namesList) {
+            System.out.println("Construtor : " + name);
+        }
+    }
+
+
+    @Override
+    public RecyclerHolder2 onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.contact, parent, false);
+        return new RecyclerHolder2(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerHolder2 holder, final int position) {
+        holder.name.setText(namesList.get(position));
+        //holder.image.setImageResource(imageList.get(position));
+        holder.designation.setText(designationList.get(position));
+        holder.call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ActivityCompat.checkSelfPermission(context, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    context.startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phoneList.get(position))));
+                    return;
+                }
+            }
+        });
+        holder.email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_SEND);
+                i.setType("message/rfc822");
+                i.putExtra(Intent.EXTRA_EMAIL, new String[]{emailList.get(position)});
+                try {
+                    v.getContext().startActivity(Intent.createChooser(i, "Send mail..."));
+                } catch (android.content.ActivityNotFoundException ex) {
+                    Toast.makeText(context, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+                }
+                //v.getContext().startActivity(new Intent(Intent.ACTION_SEND, Uri.parse(emailList.get(position))));
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        //System.out.println("size is - " + namesList.size());
+        return namesList.size();
+    }
+
+    public class RecyclerHolder2 extends RecyclerView.ViewHolder {
+
+        TextView name, designation;
+        SimpleDraweeView image;
+        ImageButton call, email;
+
+        public RecyclerHolder2(View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.contact_name);
+            image = itemView.findViewById(R.id.contact_picture);
+            designation = itemView.findViewById(R.id.contact_designation);
+            call = itemView.findViewById(R.id.call);
+            email = itemView.findViewById(R.id.email);
+            //itemView.setOnClickListener(this);
+        }
+
+    }
+}
